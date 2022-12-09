a8489af38c217851513c4c21947f5968f9048711
Firinor <tarmz@rambler.ru>
2022-12-09 20:08:11 +0300

    Boss battle UI

---
+++ Puzzles/BossBattle/MagicBulletOperator.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Puzzle.BossBattle
+{
+    public class MagicBulletOperator : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    {
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
