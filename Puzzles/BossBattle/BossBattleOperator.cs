297a7968c3831258bb2d51ee67a057a82237d247
Firinor <tarmz@rambler.ru>
2022-12-10 19:42:13 +0300

    Boss battle bullets

--- Puzzles/BossBattle/BossBattleOperator.cs
+++ Puzzles/BossBattle/BossBattleOperator.cs
@@ -1,13 +1,49 @@
+using FirUnityEditor;
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Puzzle.BossBattle
 {
     public class BossBattleOperator : PuzzleOperator
     {
-        private int bossHealth = 3;
-        private int heroHealth = 3;
+        [SerializeField]
+        private int bossMaxHealth = 3;
+        [SerializeField]
+        private int heroMaxHealth = 3;
+        private int bossHealth;
+        private int heroHealth;
+        [SerializeField, NullCheck]
+        private Slider bossHealthSlider;
+        [SerializeField, NullCheck]
+        private Slider heroHealthSlider;
+
         private float bossSpeed = 1;
+        private float bossSpeedCooldown = 1;
         private float heroSpeed = 1;
+        private float heroSpeedCooldown = 1;
+
+        [SerializeField, NullCheck]
+        private RectTransform bossBulletParent;
+        [SerializeField, NullCheck]
+        private RectTransform heroBulletParent;
+        [SerializeField, NullCheck]
+        private GameObject bossBulletPrefab;
+        [SerializeField, NullCheck]
+        private GameObject heroBulletPrefab;
+
+        void OnEnable()
+        {
+            SetHP(bossHealthSlider, ref bossHealth, bossMaxHealth);
+            SetHP(heroHealthSlider, ref heroHealth, heroMaxHealth);
+        }
+
+        private void SetHP(Slider slider, ref int currentHP, int value)
+        {
+            slider.maxValue = value;
+            slider.value = value;
+            currentHP = value;
+        }
 
         public void SetPuzzleInformationPackage(BossBattlePackage bossBattlePackage)
         {
@@ -18,6 +54,28 @@ namespace Puzzle.BossBattle
             SetVictoryEvent(bossBattlePackage.successPuzzleAction);
             SetFailEvent(bossBattlePackage.failedPuzzleAction);
             SetBackground(bossBattlePackage.PuzzleBackground);
+        }
+
+        internal void DamagePlayer()
+        {
+            heroHealth--;
+            heroHealthSlider.value = heroHealth;
+
+            if(heroHealth <= 0)
+            {
+                LosePuzzle();
+            }
+        }
+
+        internal void DamageBoss()
+        {
+            bossHealth--;
+            bossHealthSlider.value = bossHealth;
+
+            if (bossHealth <= 0)
+            {
+                SuccessfullySolvePuzzle();
+            }
         }
     }
 }
--- Puzzles/BossBattle/MagicBulletOperator.cs
+++ Puzzles/BossBattle/MagicBulletOperator.cs
@@ -1,3 +1,5 @@
+using FirUnityEditor;
+using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -5,14 +7,61 @@ namespace Puzzle.BossBattle
 {
     public class MagicBulletOperator : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
     {
+        private BossBattleOperator bossBattleOperator;
+
+        [SerializeField, NullCheck]
+        private RectTransform rectTransform;
+
+        private bool expansion = true;
+        [SerializeField]
+        private float expansionSpeed = 100;
+        [SerializeField]
+        private float reductionSpeed = 300;
+        [SerializeField]
+        private float damageSize = 500;
+        [SerializeField]
+        private float attenuationSize = 75;
+
+        public void setBossBattleOperator(BossBattleOperator bossBattleOperator)
+        {
+            this.bossBattleOperator = bossBattleOperator;
+        }
+
+        void Update()
+        {
+            if (expansion)
+            {
+                rectTransform.sizeDelta += Vector2.one * Time.deltaTime * expansionSpeed;
+            }
+            else
+            {
+                rectTransform.sizeDelta -= Vector2.one * Time.deltaTime * reductionSpeed;
+            }
+
+            if(rectTransform.sizeDelta.x < attenuationSize)
+            {
+                Destroy(gameObject);
+            }
+            if (rectTransform.sizeDelta.x > damageSize)
+            {
+                DamagePlayer();
+                Destroy(gameObject);
+            }
+        }
+
+        private void DamagePlayer()
+        {
+            bossBattleOperator.DamagePlayer();
+        }
+
         public void OnPointerEnter(PointerEventData eventData)
         {
-            throw new System.NotImplementedException();
+            expansion = false;
         }
 
         public void OnPointerExit(PointerEventData eventData)
         {
-            throw new System.NotImplementedException();
+            expansion = true;
         }
     }
 }
