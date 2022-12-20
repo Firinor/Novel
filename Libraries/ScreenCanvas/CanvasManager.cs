be78e5dfa15abc9abf7eea4eab8f5cdd7205eb1e
Firinor <tarmz@rambler.ru>
2022-12-20 18:08:55 +0300

    Global canvas scaler begining

---
+++ Libraries/ScreenCanvas/CanvasManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasManager : SinglBehaviour<CanvasManager>
+{
+    [SerializeField]
+    private Canvas mainCanvas;
+    [SerializeField]
+    private CanvasScaler mainCanvasScaler;
+
+    public static float ScreenHeight { get => instance.mainCanvasScaler.referenceResolution.y; }
+    public static float ScreenWeight { get => instance.mainCanvasScaler.referenceResolution.x; }
+}
