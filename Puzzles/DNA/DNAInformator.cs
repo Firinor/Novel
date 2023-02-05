36355e08e7cbe22e8700e0c670297d708406b940
Firinor <tarmz@rambler.ru>
2023-02-05 14:03:52 +0300

    DNA puzzle start

---
+++ Puzzles/DNA/DNAInformator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Puzzle.DNA
+{
+    public enum Nucleotide
+    {
+        A, C, G, T
+    }
+
+    public class DNAInformator
+    {
+
+    }
+}
