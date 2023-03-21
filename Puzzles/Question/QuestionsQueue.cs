9e0a2d830776aff4946a429c4716953643daad3f
Firinor <tarmz@rambler.ru>
2023-03-21 20:34:15 +0300

    Queue questions

---
+++ Puzzles/Question/QuestionsQueue.cs
@@ -0,0 +1,8 @@
+using Puzzle.TetraQuestion;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestionsQueue : MonoBehaviour
+{
+    private List<Question> questionsQueue = new List<Question>();
+}
